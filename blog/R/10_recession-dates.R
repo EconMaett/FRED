6bfb9c1d21b10801b03d965350106e0e98c5a241
EconMaett <matthias.spichiger@bluewin.ch>
2024-03-28 14:39:36 +0100

    update

--- blog/R/10_recession-dates.R
+++ blog/R/10_recession-dates.R
@@ -1,2 +1,74 @@
 # 10 - Daily recession dates in FRED ----
 # URL: https://fredblog.stlouisfed.org/2022/06/daily-recession-dates-in-fred/
+library(tidyverse)
+library(fredr)
+fig_path <- "blog/figures/"
+# Recessions have beginnings and ends.
+
+# In the U.S., these are determined by the Business Cycle Dating Committee
+# of the National Bureau of Economic Research (NBER).
+
+# They announce the months and quarters when overall economic activity has reached a peak 
+# and starts to contract and when it reached a trough and starts expanding again.
+
+# The NBER determined the beginning and end of the COVID-19 recession as
+# February 2020 and April 2020, respectively.
+
+# The Federal Reserve Bank of St. Louis has created *daily*
+# data series to help us better understand the possible choices for
+# dating recessions.
+
+# We can create a graph that shows the start and end of the COVID-19 recession
+# with three series of recession dates.
+
+# The series have a value of 1 when the economy is in a recession and 0 otherwise.
+
+# The three series report the same total number of days (60 days) during which
+# overall economic activity was contracting.
+
+
+## NBER based Recession Indicators for the United States ----
+
+# - USRECDP: (Blue)   From the Peak through the Period preceding the Trough
+
+# - USRECDM: (Yellow) From the Peak through the Trough
+
+# - USRECD: (Red)     From the Period following the Peak through the Trough
+
+
+
+df <- map_dfr(.x = c("USRECDP", "USRECDM", "USRECD"), .f = fredr)
+
+df |> 
+  select(1:3) |> 
+  mutate(
+    series_id = case_when(
+      series_id == "USRECDP" ~ "From the Peak through the Period preceding the Trough", 
+      series_id == "USRECDM" ~ "From the Peak through the Trough", 
+      series_id == "USRECD" ~ "From the Period following the Peak through the Trough"
+    ),
+    series_id = factor(series_id)
+  ) |> 
+  filter(year(date) > 2019 & date <= date("2020-06-01")) |> 
+  ggplot(mapping = aes(x = date, y = value, color = series_id)) +
+  geom_line(lwd = 1.2) +
+  scale_color_manual(values = c("blue", "yellow", "red")) +
+  labs(
+    title = "NBER based Recession Indicators for the United States",
+    caption = "Source: Federal Reserve Bank of St. Louis",
+    x = NULL, y = NULL
+  ) +
+  theme_minimal() +
+  scale_y_continuous(breaks = c(0, 1)) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%Y-%M") +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "top",
+    legend.text = element_text(size = 12),
+    legend.direction = "vertical"
+    )
+
+ggsave(filename = "10_recession-dates.png", path = fig_path, height = 8, width = 12, bg = "white")
+graphics.off()
+
+# END
--- blog/R/71_recession-dates.R
+++ blog/R/71_recession-dates.R
@@ -1,5 +1,129 @@
-# 71 - Recession dating ----
+# 71 - Discrepancies in dating recessions ----
 # URL: https://fredblog.stlouisfed.org/2021/08/discrepancies-in-dating-recessions/
+library(tidyverse)
+library(fredr)
+library(zoo)
+fig_path <- "blog/figures/"
+# How the COVID-19 recession lasted only two months but spanned
+# two quarters.
+
+# There is no hard and fast rule to determine when the U.S. economy has
+# entered a recession, and there is no one indicator that determines a recession.
+
+# Most recently, the NBER's recession dating committee identified
+# February 2020 as the latest business cycle peak and April 2020 as the trough.
+# This is the shortest recession in US history - 
+# just 2 to 3 months.
+
+# The committee also releases the corresponding *quarters* of the peak and trough.
+# These turning points are primarily based on quarterly averages of the monthly indicators,
+# along with prominent quarterly series such as real GDP.
+
+# In most instances, the turning point quarters match the turning point months.
+
+# In fact, the months and quarters *had* all been in alignment since March (Q1) 1954.
+
+# But for the COVID-19 recession, the months and quarters of the business cycle turning points do not align.
 
 
+# The FRED team applies recession shading starting with the month of the peak and ending with the month prior to the trough.
+# This method has most accurately aligned with the turning points in economic data
+# because of the consistency between NBER turning point months and quarters.
 
+# https://fred.stlouisfed.org/graph/?g=FYxH
+
+# - USRECM. Monthly: NBER based recession indicators for the US from the Peak through the Trough.
+# - USRECQM. Quarterly: NBER based recession indicators for the US from the Peak through the Trough.
+
+
+## 1) Plot the monthly and quarterly NBER recession dates ----
+params <- list(
+  series_id = c("USRECM", "USRECQM"),
+  frequency = c("m", "q")
+)
+
+df <- pmap_df(.l = params, .f = ~ fredr(series_id = .x, frequency = .y)) |> 
+  select(1:3) |> 
+  filter(year(date) >= 2019 & year(date) < 2021)
+
+df |> 
+  mutate(
+    series_id = case_when(
+      series_id == "USRECM" ~ "Monthly",
+      series_id == "USRECQM" ~ "Quarterly"
+    ),
+    series_id = factor(series_id)
+  ) |> 
+  ggplot(mapping = aes(x = date, y = value, color = series_id)) +
+  geom_step(lwd = 1.2) +
+  scale_color_manual(values = c("darkblue", "firebrick")) +
+  labs(
+    title = "NBER based Recession Indicators for the United States",
+    subtitle = "From the Peak through the Trough", 
+    caption = "Source: Federal Reserve Bank of St. Louis",
+    x = NULL, y = NULL
+  ) +
+  scale_y_continuous(breaks = c(0, 1)) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%Y-%m") +
+  theme_minimal() +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "top",
+    legend.text = element_text(size = 12),
+    legend.direction = "horizontal"
+  )
+
+ggsave(filename = "71_recession-dates.png", path = fig_path, height = 8, width = 12, bg = "white")
+graphics.off()
+
+
+## 2) Plot the monthly and quarterly employment level ----
+# Change in thousands of persons
+# CE16OV
+df_m <- fredr(series_id = "CE16OV", frequency = "m", units = "chg") |> 
+  select(1:3) |> 
+  filter(year(date) >= 2019 & year(date) <= 2021)|> 
+  mutate(series_id = "CE16OV_m")
+
+df_m
+
+
+df_q <- fredr(series_id = "CE16OV", frequency = "q", units = "chg") |> 
+  select(1:3) |> 
+  filter(year(date) >= 2019 & year(date) <= 2021)|> 
+  mutate(series_id = "CE16OV_q")
+
+df_q
+
+bind_rows(df_m, df_q) |> 
+  mutate(
+    series_id = case_when(
+      series_id == "CE16OV_m" ~ "Monthly",
+      series_id == "CE16OV_q" ~ "Quarterly"
+    ),
+    series_id = factor(series_id)
+  ) |> 
+  ggplot(mapping = aes(x = date, y = value, color = series_id)) +
+  geom_hline(yintercept = 0, lwd = 1, color = "black") +
+  geom_line(lwd = 1.2) +
+  scale_color_manual(values = c("darkblue", "firebrick")) +
+  labs(
+    title = "United States: Employment Level",
+    subtitle = "Change, Thousands of Persons",
+    caption = "Source: U.S. Bureau of Labor Statistics",
+    x = NULL, y = NULL
+  ) +
+  scale_y_continuous(breaks = seq(from = -24000, to = 12000, by = 4000), limits = c(-24000, 12000)) +
+  scale_x_date(date_breaks = "3 months", date_labels = "%Y-%m", limits = c(date("2019-01-01"), date("2021-07-30"))) +
+  theme_minimal() +
+  theme(
+    legend.title = element_blank(),
+    legend.position = "top",
+    legend.text = element_text(size = 12),
+    legend.direction = "horizontal"
+  )
+
+ggsave(filename = "71_employment-levels.png", path = fig_path, height = 8, width = 12, bg = "white")
+graphics.off()
+
+# END
